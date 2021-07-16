66688a7aee920e7b06073afd110ad936b1a16764
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-07-16 13:25:18 -0500

    initial commit

---
+++ shopping.js
@@ -0,0 +1,47 @@
+//declaring variable to contain array of objects (shopping list)
+const shoppingList = [
+    {
+        id: 1,
+        name: "Milk",
+        price: 3.99,
+        buyAt: "Aldi",
+        perishable: true
+    },
+    {
+        id: 2,
+        name: "Eggs",
+        price: 0.79,
+        buyAt: "Aldi",
+        perishable: true
+    },
+    {
+        id: 3,
+        name: "Chicken Stock",
+        price: 1.09,
+        buyAt: "Aldi",
+        perishable: false
+    },
+    {
+        id: 4,
+        name: "Blueberries",
+        price: 8.99,
+        buyAt: "Costco",
+        perishable: true
+    },
+    {
+        id: 5,
+        name: "Canned Peaches",
+        price: 0.79,
+        buyAt: "Aldi",
+        perishable: false
+    },
+    {
+        id: 6,
+        name: "Sparkling Water",
+        price: 2.99,
+        buyAt: "Kroger",
+        perishable: false
+    },
+]
+
+console.log(shoppingList);
